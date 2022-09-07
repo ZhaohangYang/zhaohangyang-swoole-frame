16cb24e05cd95cfc0065a15baf377b3f01f24443
ZhaohangYang <804142454@qq.com>
2022-09-07 11:05:14 +0800

    新增配置文件实例

---
+++ config/redis.example.php
@@ -0,0 +1,13 @@
+
+<?php
+return [
+    'default' => [
+        'ip'       => '127.0.0.1',
+        'port'     => 6379,
+        'password' => '',
+        'db_index' => 0,
+        'time_out' => 1,
+        'number'   => 5,
+    ],
+
+];

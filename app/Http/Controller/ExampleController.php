280294cc6c22aeedbd8ff44580f986b3f18fd3b3
ZhaohangYang <804142454@qq.com>
2023-02-22 10:24:38 +0800

    新增工具函数

--- app/Http/Controller/ExampleController.php
+++ app/Http/Controller/ExampleController.php
@@ -8,7 +8,5 @@ class IndexController extends BasicController
 {
     public function test($params)
     {
-        $huobanBasic = \App\Application::getContainer()->get('huobanBasic');
-        $redisBasic  = \App\Application::getContainer()->get('redisBasic');
     }
 }
